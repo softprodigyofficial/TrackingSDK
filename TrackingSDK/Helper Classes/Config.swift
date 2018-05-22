f0133d2e28251a496a565d6667cdd5702495b05e
ramesh-ios <ramesh.softprodigy@gmail.com>
2018-05-23 00:18:59 +0530

    Initial commit

---
+++ TrackingSDK/Helper Classes/Config.swift
@@ -0,0 +1,20 @@
+//
+//  Config.swift
+//  EmallApp
+//
+//  Created by MSS Softprodigy on 14/12/17.
+//  Copyright © 2017 Navjot Sharma. All rights reserved.
+//
+
+import Foundation
+
+// APIs
+class Config {
+    /*
+     http://production.mgpdjtcr8a.us-east-1.elasticbeanstalk.com/event -> http://production.mgpdjtcr8a.us-east-1.elasticbeanstalk.com/v1/event
+     */
+    //static let BASE_URL = "http://production.mgpdjtcr8a.us-east-1.elasticbeanstalk.com"
+    static let BASE_URL = "http://production.mgpdjtcr8a.us-east-1.elasticbeanstalk.com/v1"
+    static let event = "/event"
+}
+
